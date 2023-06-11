cd97956c120a04d89b82717c5bc92788a8a8a61a
hmoisesgb <h.moises.gb@gmail.com>
2023-06-10 23:41:10 -0600

    Updated prep assignments code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int number = random.Next(1,101);
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess =  int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int usernumber = -1;
+
+        while (usernumber != 0)
+        {
+            Console.WriteLine("Enter a number (0 to quit): ");
+            usernumber = int.Parse(Console.ReadLine());
+
+            if (usernumber != 0)
+            {
+                numbers.Add(usernumber);
+            }
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        double average = ((double)sum) / numbers.Count;
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
