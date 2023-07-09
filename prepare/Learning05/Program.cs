abebf80a9b22b730eae849647248c610d67ba7f3
hmoisesgb <h.moises.gb@gmail.com>
2023-07-08 22:51:48 -0600

    Completed W5 Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("Blue",2);
+        shapes.Add(square);
+        
+        Rectangle rectangle = new Rectangle("Red",2,4);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Yellow",5);
+        shapes.Add(circle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The color of the shape is {color} and has an area of {area}");
+        }
     }
 }
